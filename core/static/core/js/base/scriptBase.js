f79d1b7694275ac7aabc9c9be85781b0d4ccad53
Henrique Ferreira <henriquef501@gmail.com>
2025-06-04 16:55:51 -0300

    att

--- core/static/core/js/base/scriptBase.js
+++ core/static/core/js/base/scriptBase.js
@@ -27,5 +27,26 @@ function temporizadorAlerta() {
     if (alert) alert.style.display = "none";
   }, 4000);
 }
+function toggleDropdown(button) {
+  const dropdown = button.nextElementSibling;
+
+  // Fecha todos os outros dropdowns
+  document.querySelectorAll(".dropdown").forEach(drop => {
+      if (drop !== dropdown) {
+          drop.style.display = "none";
+      }
+  });
+
+  // Alterna o dropdown atual
+  dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+
+  // Adiciona o listener global só uma vez
+  document.addEventListener("click", function handleClickOutside(event) {
+      if (!button.contains(event.target) && !dropdown.contains(event.target)) {
+          dropdown.style.display = "none";
+          document.removeEventListener("click", handleClickOutside); // Remove após esconder
+      }
+  });
+}
 
 temporizadorAlerta()
--- core/views/dashboard_views.py
+++ core/views/dashboard_views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from core.models import Paciente, Especialidade,Profissional, Servico,PacotePaciente,Agendamento,Pagamento, ESTADO_CIVIL, MIDIA_ESCOLHA, VINCULO, COR_RACA, UF_ESCOLHA,SEXO_ESCOLHA, CONSELHO_ESCOLHA
 from datetime import date, datetime, timedelta
 from django.utils import timezone
-
+FINALIZADOS = ['finalizado','desistencia','desistencia_remarcacao','falta_remarcacao','falta_cobrada']
+PENDENTES = ['pre','agendado']
 @login_required(login_url='login')
 def dashboard_view(request):
     agendamentos = Agendamento.objects.filter(data=date.today()).select_related('especialidade')
@@ -13,14 +14,20 @@ def dashboard_view(request):
     hoje = timezone.now().date()
     inicio_semana = hoje - timedelta(days=hoje.weekday())
     fim_semana = inicio_semana + timedelta(days=6)
-    agendamentos_semana = Agendamento.objects.filter(data__gte=inicio_semana, data__lte=fim_semana)
-    print(total_pacientes_ativos)
+    agendamentos_semana = Agendamento.objects.filter(data__gte=inicio_semana, data__lte=fim_semana).count()
+    agendamentos_dia = Agendamento.objects.filter(data=date.today()).count()
+    agendamentos_dia_finalizados = Agendamento.objects.filter(data=date.today(), status__in=FINALIZADOS).count()
+    agendamentos_dia_pendentes = Agendamento.objects.filter(data=date.today(), status__in=PENDENTES).count()
+    print(total_pacientes_ativos, agendamentos_semana)
     
     context = {
         'agendamentos':agendamentos,
         'total_pacientes_ativos':total_pacientes_ativos,
         'total_profissionais_ativos': total_profissionais_ativos,
         'agendamentos_semana':agendamentos_semana,
+        'agendamentos_dia':agendamentos_dia,
+        'agendamentos_dia_finalizados':agendamentos_dia_finalizados,
+        'agendamentos_dia_pendentes':agendamentos_dia_pendentes,
     }
     return render(request, 'core/dashboard.html', context)
 
