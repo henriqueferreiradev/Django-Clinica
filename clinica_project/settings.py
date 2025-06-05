ef8ccde7722b480398067bca86657d0689041ecd
Henrique Ferreira <henriquef501@gmail.com>
2025-06-05 09:06:20 -0300

    teste prod render

--- clinica_project/settings.py
+++ clinica_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','https://django-clinica-emb0.onrender.com']
+ALLOWED_HOSTS = ['https://django-clinica-emb0.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
