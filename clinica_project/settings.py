41bb7f92c4bdda6f8b3583ef5825dfaaa979bb27
Henrique Ferreira <henriquef501@gmail.com>
2025-06-05 14:34:08 -0300

    teste prod render

--- clinica_project/settings.py
+++ clinica_project/settings.py
@@ -76,18 +76,16 @@ WSGI_APPLICATION = 'clinica_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '/var/lib/postgresql/data/pgdata',
-        'USER': 'postgres',
-        'PASSWORD': 'iyRwTlygotrvOvtwZkETfUibrCzqbPVX',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
