b30b8591c3fbb1b77c096cb2ebfa060360f3e145
Henrique Ferreira <henriquef501@gmail.com>
2025-06-05 09:52:01 -0300

    teste prod render

--- clinica_project/settings.py
+++ clinica_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,6 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'core' / 'static',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,8 @@ from core.views import (
     pacientes_views,
     profissionais_views
 )
- 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('login/', auth_views.login_view, name='login'),
@@ -48,3 +49,6 @@ urlpatterns = [
  
  
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
