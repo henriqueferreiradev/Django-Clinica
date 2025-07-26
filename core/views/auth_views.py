95fa1e3866c40785891ce19a4f173ac49aa3ced4
Henrique Ferreira <henriquef501@gmail.com>
2025-07-26 08:54:29 -0300

    testes em producao

--- core/views/auth_views.py
+++ core/views/auth_views.py
@@ -1,22 +1,20 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login,logout
 from django.contrib import messages
-<<<<<<< HEAD
 from django.http import HttpResponse
 from django.core.management import call_command
 from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
 
 def setup(request):
     call_command('migrate')
-=======
-from django.shortcuts import render, redirect
->>>>>>> main
 
     User = get_user_model()
     if not User.objects.filter(username="henri").exists():
         User.objects.create_superuser("henri", "henriquef501@gmail.com", "5224")
+        return HttpResponse("Migração e superuser criados!")
 
-    return HttpResponse("Migração e superuser criados!")
+
 def login_view(request):
     form = AuthenticationForm(request, data=request.POST or None)
 
