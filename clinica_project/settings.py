d5c274ca04e60e50af819f7bcc843fcb73800965
Henrique Ferreira <henriquef501@gmail.com>
2025-10-10 15:59:18 -0300

    att

--- clinica_project/settings.py
+++ clinica_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.1.2','pontoeqsys.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['http://192.168.1.2']
