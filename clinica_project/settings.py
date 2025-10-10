bbace8feb2c015a40ed07c51bc12f1ae899f11c3
Henrique Ferreira <henriquef501@gmail.com>
2025-10-10 15:38:46 -0300

    att

--- clinica_project/settings.py
+++ clinica_project/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['pontoeqsys.onrender.com']
-CSRF_TRUSTED_ORIGINS  = ['https://django-clinica-production.up.railway.app']
+ALLOWED_HOSTS = ['192.168.1.2','pontoeqsys.onrender.com']
+ 
 
 
 # Application definition
