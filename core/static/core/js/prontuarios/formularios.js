27b5178a2887639004d336d1d31ee8674a3ec3ea
Henrique Monteiro <henriquef501@gmail.com>
2025-10-24 18:05:44 -0300

    att

---
+++ core/static/core/js/prontuarios/formularios.js
@@ -0,0 +1,9 @@
+async function salvarProntuario() {
+    const dados = {
+        paciente_id: document.getElementById('pacienteId').value,
+        profissional_id: document.getElementById('profissionalId').value,
+        agendamento_id: document.getElementById('agendamentoId').value,
+        queixa_principal: document.getElementById('queixaPrincipal').value,
+        historia_doenca: document.getElementById('historiaDoenca').value,
+    }
+}
